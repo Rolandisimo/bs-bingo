08709e95b4fe9f78af9c2c47fac555101598dc04
Rolands Jegorovs <rolandjegorov@gmail.com>
2020-12-17 14:44:06 +0200

    Updates

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-declare interface TypedEvent<T extends Element> extends Event {
-  target: T;
-  currentTarget: T;
-}
-
-declare interface TypedMouseEvent<T extends Element> extends MouseEvent {
-  target: EventTarget & T;
-  currentTarget: EventTarget & T;
-}
